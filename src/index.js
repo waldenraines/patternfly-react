12adca710f752583375cf43b2d91801996149b61
Patrick Riley <priley86@gmail.com>
2017-05-19 22:56:28 -0400

    feat(list-view): add list-view w/ expansion
    Closes #30

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import Alert from './Alert/Alert'
-export { Alert }
+export { default as Alert } from './Alert/Alert'
+export { default as ListView } from './ListView/ListView'
+export { default as ListViewItem } from './ListView/ListViewItem'
+export {
+  default as ListViewItemContainer
+} from './ListView/ListViewItemContainer'
+export {
+  default as ListViewItemExpansion
+} from './ListView/ListViewItemExpansion'
