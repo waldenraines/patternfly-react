5e5373d5f00db59d693c373bbec8045f2a56690c
Walden Raines <walden@redhat.com>
2017-06-07 10:14:53 +0300

    feat(list-view): add basic list view with and without expansion
    Adds a first pass at expandable and non-expandable list view based on patternfly's markup.
    
    Closes #30

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ export { default as Alert } from './Alert/Alert'
 export { default as ListView } from './ListView/ListView'
 export { default as ListViewItem } from './ListView/ListViewItem'
 export {
-  default as ListViewItemContainer
-} from './ListView/ListViewItemContainer'
-export {
-  default as ListViewItemExpansion
-} from './ListView/ListViewItemExpansion'
+  default as ListViewExpandableItem
+} from './ListView/ListViewExpandableItem'
